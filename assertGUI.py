b6d7d09027fbd632a52ae7e05ae482eb0d8c82ba
Michael Skoufis <skoufis@stanford.edu>
2025-11-24 12:53:11 -0800

    Reorg repo structure

---
+++ assertGUI.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# Title      : Configuration file for ASSERT event display
+#-----------------------------------------------------------------------------
+# Borrowed from ePixGUI.py (ePixViewer, https://github.com/slaclab/ePixViewer)
+
+import os
+import pydm
+
+def runReceiverDisplay(dataReceiver, serverList='localhost:9090', root=None,
+                   title=None,sizeX=800,sizeY=1000,maxListExpand=5,maxListSize=100):
+
+    #pyrogue.pydm.runPyDM()
+
+    if root is not None:
+
+        if not root.running:
+            raise Exception("Attempt to use pydm with root that has not started")
+
+        os.environ['ROGUE_SERVERS'] = 'localhost:{}'.format(root.serverPort)
+    else:
+        os.environ['ROGUE_SERVERS'] = serverList
+
+    ui = os.path.abspath(__file__)
+
+    if title is None:
+        title = "Assert Live Event Display: {}".format(os.getenv('ROGUE_SERVERS'))
+
+    args = []
+    args.append(f"sizeX={sizeX}")
+    args.append(f"sizeY={sizeY}")
+    args.append(f"title='{title}'")
+    args.append(f"maxListExpand={maxListExpand}")
+    args.append(f"maxListSize={maxListSize}")
+
+    macrosA = {}
+    macrosA['dataReceiver'] = dataReceiver
+    macrosA['title'] = title
+    macrosA['sizeX'] = sizeX
+    macrosA['sizeY'] = sizeY
+    app = pydm.PyDMApplication(ui_file=ui,
+                               command_line_args=args,
+                               macros=macrosA,
+                               hide_nav_bar=True,
+                               hide_menu_bar=True,
+                               hide_status_bar=True)
+    app.exec()
+
+class assertGUI(pydm.Display):
+    def __init__(self, parent=None, args=None, macros=None):
+        super().__init__(parent=parent, args=args, macros=macros)
+        # print(f'{macros=}')
+        self._dataReceiver = macros['dataReceiver']
+        self.ui.PyDMImageView.newImageSignal.connect(self.updateDisplay)
+        self.ui.PyDMImageView.scene.sigMouseClicked.connect(self.clickProcess)
+        # self.ui.PyDMLineEdit_3.textChanged.connect(self.resetTimePlot)
+        # self.ui.PyDMLineEdit_6.textChanged.connect(self.resetTimePlot)
+        # self.ui.lineEdit.textChanged.connect(self.setTimeSpan)
+        # self.ui.PyDMCheckbox_15.stateChanged.connect(self.resetTimePlot)
+        # self.ui.pushButton.clicked.connect(self.resetTimePlot)
+        self.sizeX = macros['sizeX']
+        self.sizeY = macros['sizeY']
+
+    def updateDisplay(self):
+        maxContrast = int(self.ui.PyDMLineEdit_5.displayText())
+        minContrast = int(self.ui.PyDMLineEdit_4.displayText())
+        self.ui.PyDMImageView.setColorMapLimits(minContrast, maxContrast)
+
+    # def setTimeSpan(self):
+    #     self.ui.PyDMTimePlot.setTimeSpan(int(self.ui.lineEdit.text()))
+
+    def clickProcess(self, event):
+        pos = self.ui.PyDMImageView.getView().getViewBox().mapSceneToView(event.scenePos())
+        if self.sizeX != 0 and int(pos.x()) > self.sizeX :
+            x = str(self.sizeX-1)
+        elif int(pos.x()) < 0 :
+            x = str(0)
+        else :
+            x = str(int(pos.x()))
+
+        if self.sizeY != 0 and  int(pos.y()) > self.sizeY :
+            y = str(self.sizeY-1)
+        elif int(pos.y()) < 0 :
+            y = str(0)
+        else :
+            y = str(int(pos.y()))
+
+        self.ui.PyDMLineEdit_2.setText(x)
+        self.ui.PyDMLineEdit_2.send_value()
+        self.ui.PyDMLineEdit_6.setText(y)
+        self.ui.PyDMLineEdit_6.send_value()
+
+    # def resetTimePlot(self):
+    #     self.ui.PyDMTimePlot.removeYChannel(self.ui.PyDMTimePlot.findCurve(
+    #         f'{self._dataReceiver}.PixelData'))
+    #     self.ui.PyDMTimePlot.addYChannel(
+    #         y_channel = f'{self._dataReceiver}.PixelData',
+    #         name = "Pixel counts",
+    #         plot_style = "Line",
+    #         color = "white",
+    #         lineWidth = 1)
+
+    def ui_filename(self):
+        # Point to the UI file
+        return 'ui/assertViewerPyDM_ppl.ui'
+
+    def ui_filepath(self):
+        # Return the full path to the UI file
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), self.ui_filename())
