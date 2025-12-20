f1b5a8f00ad60d8483d04abb4d3d4de2983fd46e
Michael Skoufis <skoufis@stanford.edu>
2025-12-19 18:17:56 -0800

    Enable plot reset and redraw

--- python/assertViewer/assertGUI.py
+++ python/assertViewer/assertGUI.py
@@ -6,7 +6,9 @@
 
 import os
 import pydm
-from qtpy.QtWidgets import QWidget, QLabel, QGridLayout, QVBoxLayout
+import numpy as np
+from pydm.widgets import PyDMLineEdit, PyDMLabel
+from qtpy.QtWidgets import QWidget, QLabel, QGridLayout, QVBoxLayout, QSpacerItem, QSizePolicy
 
 def runReceiverDisplay(dataReceiver, serverList='localhost:9090', root=None,
                    title=None,sizeX=800,sizeY=1000,maxListExpand=5,maxListSize=100):
@@ -51,37 +53,53 @@ class assertGUI(pydm.Display):
     def __init__(self, parent=None, args=None, macros=None):
         super().__init__(parent=parent, args=args, macros=macros)
         # print(f'{macros=}')
+        self._asics = 8
         self._dataReceiver = macros['dataReceiver']
 #        self.ui.PyDMImageView.newImageSignal.connect(self.updateDisplay)
 #        self.ui.PyDMImageView.scene.sigMouseClicked.connect(self.clickProcess)
-        # self.ui.PyDMLineEdit_3.textChanged.connect(self.resetTimePlot)
-        # self.ui.PyDMLineEdit_6.textChanged.connect(self.resetTimePlot)
+        # self.ui.PyDMLineEdit_3.textChanged.connect(self.resetPlots)
+        # self.ui.PyDMLineEdit_6.textChanged.connect(self.resetPlots)
         # self.ui.lineEdit.textChanged.connect(self.setTimeSpan)
-        # self.ui.PyDMCheckbox_15.stateChanged.connect(self.resetTimePlot)
-        # self.ui.pushButton.clicked.connect(self.resetTimePlot)
+        # self.ui.PyDMCheckbox_15.stateChanged.connect(self.resetPlots)
+        # self.ui.pushButton.clicked.connect(self.resetPlots)
         self.sizeX = macros['sizeX']
         self.sizeY = macros['sizeY']
-        self.setup_update()
-        self.setup_display()
+        self.setup_main_tab()
+        self.setup_config_tab()
 
-    def setup_update(self):
-        #self.ui.PyDMLineEdit_3.textChanged.connect(self.resetTimePlot)
-        #self.ui.PyDMLineEdit.channel=f'{self._dataReceiver}.ASIC0FrameCnt'
+    def setup_main_tab(self):
         grid_layout=QGridLayout()
-        label=QLabel(f'Frame Count 2')
-        #lineEdit=PyDMLineEdit(init_channel=f'{self._dataReceiver}.ASIC0FrameCnt')
-        #lineEdit=PyDMLineEdit()
-        grid_layout.addWidget(label,1,0)
-        #grid_layout.addWidget(lineEdit,1,1)
+        spacer_label=QLabel('')
+        frame_cnt_label=QLabel(f'Frame Count')
+        frame_cnt_line=PyDMLineEdit(init_channel=f'{self._dataReceiver}.ASIC0FrameCnt')
+        #frame_cnt_line.textChanged.connect(self.resetPlots)
+        frame_cnt_line.textChanged.connect(self.onClick_updatePlots)
+        vertical_spacer=QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+        grid_layout.addWidget(spacer_label,0,0)
+        grid_layout.addWidget(frame_cnt_label,1,0)
+        grid_layout.addWidget(frame_cnt_line,1,1)
+        grid_layout.addItem(vertical_spacer,2,0)
         self.ui.PyDMTabWidget_main.setLayout(grid_layout)
-        pass
 
-    def setup_display(self):
+    def setup_config_tab(self):
+        self.ui.PyDMCheckbox_noise.setChecked(True)
+        self.ui.PyDMCheckbox_pedestals.setChecked(True)
+        self.ui.PyDMCheckbox_cm.setChecked(True)
+
         self.ui.PyDMCheckbox_noise.clicked.connect(self.onClick_updatePlots)
         self.ui.PyDMCheckbox_pedestals.clicked.connect(self.onClick_updatePlots)
         self.ui.PyDMCheckbox_cm.clicked.connect(self.onClick_updatePlots)
 
+    def get_attribute_dynamically(self, attribute_name):
+        # Accessing an instance variable dynamically
+        instance_attr = getattr(self, attribute_name, "Attribute not found")
+        return instance_attr
+
     def onClick_updatePlots(self):
+        print('Update the plots ...\n')
+
+        self.resetPlots()
+
         # Read current plot settings and update plots
         noise_checkbox     = self.ui.PyDMCheckbox_noise 
         pedestals_checkbox = self.ui.PyDMCheckbox_pedestals
@@ -96,17 +114,59 @@ class assertGUI(pydm.Display):
 
         # Depending on the configuration, update the curves with the appropriate counts  
         if noise_checkbox.isChecked() and pedestals_checkbox.isChecked() and cm_checkbox.isChecked():
-            pass
+            self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC0SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.ASIC1SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC2SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_4.addChannel(y_channel = f'{self._dataReceiver}.ASIC3SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_5.addChannel(y_channel = f'{self._dataReceiver}.ASIC4SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_6.addChannel(y_channel = f'{self._dataReceiver}.ASIC5SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_7.addChannel(y_channel = f'{self._dataReceiver}.ASIC6SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_8.addChannel(y_channel = f'{self._dataReceiver}.ASIC7SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
         elif noise_checkbox.isChecked() and pedestals_checkbox.isChecked():
-            pass
+            self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC0SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.ASIC1SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC2SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_4.addChannel(y_channel = f'{self._dataReceiver}.ASIC3SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_5.addChannel(y_channel = f'{self._dataReceiver}.ASIC4SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_6.addChannel(y_channel = f'{self._dataReceiver}.ASIC5SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_7.addChannel(y_channel = f'{self._dataReceiver}.ASIC6SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_8.addChannel(y_channel = f'{self._dataReceiver}.ASIC7SigNosPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
         elif noise_checkbox.isChecked() and cm_checkbox.isChecked():
-            pass
+            self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC0SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.ASIC1SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC2SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_4.addChannel(y_channel = f'{self._dataReceiver}.ASIC3SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_5.addChannel(y_channel = f'{self._dataReceiver}.ASIC4SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_6.addChannel(y_channel = f'{self._dataReceiver}.ASIC5SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_7.addChannel(y_channel = f'{self._dataReceiver}.ASIC6SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_8.addChannel(y_channel = f'{self._dataReceiver}.ASIC7SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
         elif pedestals_checkbox.isChecked() and cm_checkbox.isChecked():
-            pass
+            self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC0SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.ASIC1SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC2SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_4.addChannel(y_channel = f'{self._dataReceiver}.ASIC3SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_5.addChannel(y_channel = f'{self._dataReceiver}.ASIC4SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_6.addChannel(y_channel = f'{self._dataReceiver}.ASIC5SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_7.addChannel(y_channel = f'{self._dataReceiver}.ASIC6SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_8.addChannel(y_channel = f'{self._dataReceiver}.ASIC7SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
         elif noise_checkbox.isChecked():
-            pass
+            self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC0SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.ASIC1SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC2SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_4.addChannel(y_channel = f'{self._dataReceiver}.ASIC3SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_5.addChannel(y_channel = f'{self._dataReceiver}.ASIC4SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_6.addChannel(y_channel = f'{self._dataReceiver}.ASIC5SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_7.addChannel(y_channel = f'{self._dataReceiver}.ASIC6SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_8.addChannel(y_channel = f'{self._dataReceiver}.ASIC7SigNos',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
         elif pedestals_checkbox.isChecked():
-            pass
+            self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC0SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.ASIC1SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC2SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_4.addChannel(y_channel = f'{self._dataReceiver}.ASIC3SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_5.addChannel(y_channel = f'{self._dataReceiver}.ASIC4SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_6.addChannel(y_channel = f'{self._dataReceiver}.ASIC5SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_7.addChannel(y_channel = f'{self._dataReceiver}.ASIC6SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
+            self.ui.PyDMWaveformPlot_8.addChannel(y_channel = f'{self._dataReceiver}.ASIC7SigPed',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3)
         elif cm_checkbox.isChecked():
             pass
         else:
@@ -143,7 +203,7 @@ class assertGUI(pydm.Display):
 #        self.ui.PyDMLineEdit_6.setText(y)
 #        self.ui.PyDMLineEdit_6.send_value()
 
-    # def resetTimePlot(self):
+    def resetPlots(self):
     #     self.ui.PyDMTimePlot.removeYChannel(self.ui.PyDMTimePlot.findCurve(
     #         f'{self._dataReceiver}.PixelData'))
     #     self.ui.PyDMTimePlot.addYChannel(
@@ -152,6 +212,17 @@ class assertGUI(pydm.Display):
     #         plot_style = "Line",
     #         color = "white",
     #         lineWidth = 1)
+         print('Reset the plots...\n')
+         #for asic in np.linspace(1,self._asics,self._asics):
+         #   self.ui.get_attribute_dynamically(f'PyDMWaveformPlot_{asic}').clearCurves()
+         self.ui.PyDMWaveformPlot_1.clearCurves()
+         self.ui.PyDMWaveformPlot_2.clearCurves()
+         self.ui.PyDMWaveformPlot_3.clearCurves()
+         self.ui.PyDMWaveformPlot_4.clearCurves()
+         self.ui.PyDMWaveformPlot_5.clearCurves()
+         self.ui.PyDMWaveformPlot_6.clearCurves()
+         self.ui.PyDMWaveformPlot_7.clearCurves()
+         self.ui.PyDMWaveformPlot_8.clearCurves()
 
     def ui_filename(self):
         # Point to the UI file
