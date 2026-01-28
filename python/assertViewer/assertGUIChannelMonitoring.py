762def5b9fc45254699f5b2e92fa07dd6421a004
Michael Skoufis <skoufis@stanford.edu>
2026-01-28 10:01:02 -0800

    Update histogram when params change

--- python/assertViewer/assertGUIChannelMonitoring.py
+++ python/assertViewer/assertGUIChannelMonitoring.py
@@ -64,10 +64,10 @@ class assertGUIChannelMonitoring(pydm.Display):
         self._asics = 8
         self._channels = 64
         self._port = macros['port']
+        self.connect_rogue_root()
         self.init_colorbar()
         self.init_crosshair()
         self.setup_main_tab()
-        self.connect_rogue_root()
         self.set_image_channels()
         self.enable_mouse_cursor()
         
@@ -78,7 +78,7 @@ class assertGUIChannelMonitoring(pydm.Display):
            self._root = client.root
         
            # Get a variable value with a read, this returns the native value
-           #ret = self._root.RogueVersion.get()
+           ret = self._root.RogueVersion.get()
            #print(f"Version = {ret}")
 
     def init_colorbar(self):
@@ -124,6 +124,19 @@ class assertGUIChannelMonitoring(pydm.Display):
         self.ui.PyDMLineEdit_5.setChannel(f'{self._dataReceiver}.MaxContrast')
         self.ui.PyDMLineEdit_5.textChanged.connect(self.updateColorMapLimits)
 
+        self.ui.PyDMLineEdit_8.setChannel(f'{self._dataReceiver}.BinsStart')
+        self.ui.PyDMLineEdit_9.setChannel(f'{self._dataReceiver}.BinsStop')
+        self.ui.PyDMLineEdit_10.setChannel(f'{self._dataReceiver}.NumBins')
+
+        self.ui.PyDMLineEdit_11.setChannel(f'{self._dataReceiver}.BinsStart')
+        self.ui.PyDMLineEdit_11.textChanged.connect(self.update_histogram_params)
+
+        self.ui.PyDMLineEdit_12.setChannel(f'{self._dataReceiver}.BinsStop')
+        self.ui.PyDMLineEdit_12.textChanged.connect(self.update_histogram_params)
+
+        self.ui.PyDMLineEdit_13.setChannel(f'{self._dataReceiver}.NumBins')
+        self.ui.PyDMLineEdit_13.textChanged.connect(self.update_histogram_params)
+
     def set_image_channels(self):
         #self.ui.PyDMImageView_1.setImageChannel(f"{self._dataReceiver}.ASIC0Image")
         #self.ui.PyDMImageView_2.setImageChannel(f"{self._dataReceiver}.ASIC1Image")
@@ -149,7 +162,6 @@ class assertGUIChannelMonitoring(pydm.Display):
         #self.ui.PyDMImageView_6.scene.sigMouseClicked.connect(self.clickProcessImage6)
         #self.ui.PyDMImageView_7.scene.sigMouseClicked.connect(self.clickProcessImage7)
         #self.ui.PyDMImageView_8.scene.sigMouseClicked.connect(self.clickProcessImage8)
-
         for i in np.arange(1,self._asics+1):
             getattr(self.ui, f'PyDMImageView_{i}').scene.sigMouseClicked.connect(getattr(self, f'clickProcessImage{i}'))
 
@@ -161,8 +173,8 @@ class assertGUIChannelMonitoring(pydm.Display):
     def updateColorMapLimits(self):
         #minContrast = int(self.ui.PyDMLineEdit_2.displayText())
         #maxContrast = int(self.ui.PyDMLineEdit_3.displayText())
-        minContrast = int(self.ui.PyDMLineEdit_2.text())
-        maxContrast = int(self.ui.PyDMLineEdit_3.text())
+        minContrast = int(self.ui.PyDMLineEdit_4.text())
+        maxContrast = int(self.ui.PyDMLineEdit_5.text())
         #self.ui.PyDMImageView_1.setColorMapLimits(minContrast, maxContrast)
         #self.ui.PyDMImageView_2.setColorMapLimits(minContrast, maxContrast)
         #self.ui.PyDMImageView_3.setColorMapLimits(minContrast, maxContrast)
@@ -176,7 +188,7 @@ class assertGUIChannelMonitoring(pydm.Display):
             getattr(self.ui, f'PyDMImageView_{i}').setColorMapLimits(minContrast, maxContrast)
 
         for i in np.arange(1,self._asics+1):
-            #getattr(self, f'self.colorbar_{i}').setLevels(values=(int(self.ui.PyDMLineEdit_2.text()), int(self.ui.PyDMLineEdit_3.text())))
+            #getattr(self, f'self.colorbar_{i}').setLevels(values=(int(self.ui.PyDMLineEdit_4.text()), int(self.ui.PyDMLineEdit_5.text())))
             getattr(self, f'self.colorbar_{i}').setLevels(values=(minContrast, maxContrast))
 
     def updateDisplay(self):
@@ -250,22 +262,28 @@ class assertGUIChannelMonitoring(pydm.Display):
         self.ui.PyDMWaveformPlot_1.addChannel(y_channel = f'{self._dataReceiver}.ASIC{sensor-1}CntHist{channel}',plot_style = "Line",color = "orange",lineWidth = 3,yAxisName = "ADC Counts")
 
     def update_channel_histogram(self, sensor, channel):
-        bin_start = int(self.ui.PyDMLineEdit_8.text())
-        bin_stop  = int(self.ui.PyDMLineEdit_9.text())
-        num_bins  = int(self.ui.PyDMLineEdit_10.text())
+        bin_start = self._root.AsicSampleProcessor.BinsStart.get()
+        bin_stop  = self._root.AsicSampleProcessor.BinsStop.get()
+        num_bins  = self._root.AsicSampleProcessor.NumBins.get()
         vals = getattr(self._root.AsicSampleProcessor, f'ASIC{sensor-1}CntHist{channel}').get()
         y, x = np.histogram(vals, bins=np.linspace(bin_start, bin_stop, num_bins))
-        self._root.AsicSampleProcessor.BinsStart.set(bin_start, write = True)
-        self._root.AsicSampleProcessor.BinsStop.set(bin_stop, write = True)
-        self._root.AsicSampleProcessor.NumBins.set(num_bins, write = True)
         self._root.AsicSampleProcessor.Bins.set(x, write = True)
         self._root.AsicSampleProcessor.Frequencies.set(y, write = True)
         self.ui.PyDMWaveformPlot_2.clearCurves()
-        self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.Frequencies',x_channel = f'{self._dataReceiver}.Bins',plot_style = "Bar",color = "orange",barWidth = 3,yAxisName = "Frequencies") 
+        self.ui.PyDMWaveformPlot_2.addChannel(y_channel = f'{self._dataReceiver}.Frequencies',x_channel = f'{self._dataReceiver}.Bins',plot_style = "Line",color = "orange",lineWidth = 3,yAxisName = "Frequencies") 
 
     def update_all_channels_plot(self, sensor):
         self.ui.PyDMWaveformPlot_3.clearCurves()
         self.ui.PyDMWaveformPlot_3.addChannel(y_channel = f'{self._dataReceiver}.ASIC{sensor-1}Sig',x_channel = f'{self._dataReceiver}.IndexChannels',plot_style = "Line",color = "orange",lineWidth = 3,yAxisName = "ADC Counts") 
+
+    def update_histogram_params(self):
+        bin_start = int(self.ui.PyDMLineEdit_11.text())
+        bin_stop  = int(self.ui.PyDMLineEdit_12.text())
+        num_bins  = int(self.ui.PyDMLineEdit_13.text())
+        self._root.AsicSampleProcessor.BinsStart.set(bin_start, write = True)
+        self._root.AsicSampleProcessor.BinsStop.set(bin_stop, write = True)
+        self._root.AsicSampleProcessor.NumBins.set(num_bins, write = True)
+        self.update_channel_histogram(int(self.ui.PyDMLineEdit_7.text()),int(self.ui.PyDMLineEdit_6.text()))
  
     def ui_filename(self):
         # Point to the UI file
